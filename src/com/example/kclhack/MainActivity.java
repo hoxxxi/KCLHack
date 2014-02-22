a97de66f7db753c9cc1bcc91c1d36730bfd021c2
k1209080 <radu.bitu@kcl.ac.uk>
2014-02-22 16:03:24 +0000

    created the custom listView

--- src/com/example/kclhack/MainActivity.java
+++ src/com/example/kclhack/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.kclhack;
 
+import java.util.ArrayList;
+
+import data_types.Football_List_Adapter;
+import data_types.Game;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
+private ArrayList<Game> gameArray;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		gameArray = new ArrayList<Game>();
+		ListView gamesList = (ListView) findViewById(R.id.gamesList);
+		populate();
+		Football_List_Adapter adapter = new Football_List_Adapter(MainActivity.this, gameArray);
+		gamesList.setAdapter(adapter);
+		
+	}
+
+	private void populate() {
+		gameArray.add(new Game("Petrolu", "Rapid", "10-0", "Petrolistii dau muie la rapid", 20));
+		gameArray.add(new Game("Chelsea", "Barcelona", "0-2", "Red card for Chelsea", 30));
+		gameArray.add(new Game("Juventus", "Real Madrid", "3-1", "Juventus fans say that real madrid are wankers", 44));
 	}
 
 	@Override
---
+++ src/data_types/Football_List_Adapter.java
@@ -0,0 +1,55 @@
+package data_types;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import com.example.kclhack.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+public class Football_List_Adapter extends ArrayAdapter<Game> {
+
+	private LayoutInflater li;
+	ArrayList<Game> gameList;
+	
+	public Football_List_Adapter(Context context, ArrayList<Game> gameList) {
+		super(context, R.layout.item_list_football,gameList);
+		li = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+		this.gameList=gameList;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View itemView = convertView;
+		if(itemView==null)
+			itemView = li.inflate(R.layout.item_list_football, parent, false);
+		//get current person
+		Game currentGame = gameList.get(position);
+		//fill views
+		ImageView leaguePic = (ImageView) itemView.findViewById(R.id.item_our_immage);
+		//profile pic should be changed here
+		TextView teamOne = (TextView) itemView.findViewById(R.id.item_TeamOne_TV);
+		TextView score = (TextView) itemView.findViewById(R.id.item_Score_TV);
+		TextView teamTwo = (TextView) itemView.findViewById(R.id.item_TeamTwo_TV);
+		TextView time = (TextView) itemView.findViewById(R.id.item_time_TV);
+		TextView latestUpdate = (TextView) itemView.findViewById(R.id.item_LatestUpdate_TV);
+		//put values
+		teamOne.setText(currentGame.getTeam_One());
+		score.setText(currentGame.getScore());
+		teamTwo.setText(currentGame.getTeam_Two());
+		time.setText(currentGame.getTime()+"\"");
+		latestUpdate.setText(currentGame.getLatestUpdate());
+		return itemView;
+	}
+	
+	
+
+}
