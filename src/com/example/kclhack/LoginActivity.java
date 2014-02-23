cf6d210841416851fbbd704f319d7ca878de0fc6
HoxXxi <hoxxxii@gmail.com>
2014-02-23 06:33:52 +0000

    GUI 4

--- src/com/example/kclhack/LoginActivity.java
+++ src/com/example/kclhack/LoginActivity.java
@@ -2,16 +2,28 @@ package com.example.kclhack;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
 
+	protected Typeface aubrey;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		//change
+		aubrey = Typeface.createFromAsset(getAssets(), "font/aubrey.TTF");
+		TextView labelUser = (TextView) findViewById(R.id.textView3);
+		TextView labelPassword = (TextView) findViewById(R.id.textView2);
+		TextView labelSignup = (TextView) findViewById(R.id.textView1);
+		labelUser.setTypeface(aubrey);
+		labelPassword.setTypeface(aubrey);
+		labelSignup.setTypeface(aubrey);
+		
 	}
 
 	@Override
